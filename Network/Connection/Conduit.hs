7702e451e797bf38705d642b3ae5f14f778e1e5d
Sebastian Dröge <sebastian@centricular.com>
2014-09-09 21:00:39 +0200

    Add documentation

--- Network/Connection/Conduit.hs
+++ Network/Connection/Conduit.hs
@@ -1,7 +1,11 @@
 {-# LANGUAGE RankNTypes #-}
+-- | A @conduit@ source and sink based on "Network.Connection" from the
+-- @connection@ package, and ResourceT aware constructors.
 module Network.Connection.Conduit
-    ( sourceConnection
+    ( -- * Source and sink
+      sourceConnection
     , sinkConnection
+      -- * ResourceT aware constructors
     , connectFromHandle
     , connectTo
     ) where
@@ -16,6 +20,8 @@ import Control.Monad.Trans.Resource
 import Control.Monad (unless)
 import System.IO (Handle)
 
+-- | Stream the data from the connection. This does not close the connection
+-- on completion
 sourceConnection :: MonadIO m => C.Connection -> Producer m ByteString
 sourceConnection connection =
     loop
@@ -25,17 +31,21 @@ sourceConnection connection =
         unless (BS.null bs) $
             yield bs >> loop
 
+-- | Stream all incoming data to the connection. This does not close the
+-- connection on completion.
 sinkConnection :: MonadIO m => C.Connection -> Consumer ByteString m ()
 sinkConnection connection =
     loop
   where
     loop = await >>= maybe (return ()) (\bs -> lift (liftIO $ C.connectionPut connection bs) >> loop)
 
+-- | Create a new connection from a handle. See 'Network.Connection.connectFromHandle'.
 connectFromHandle :: (MonadResource m) => C.ConnectionContext -> Handle -> C.ConnectionParams -> m C.Connection
 connectFromHandle ctx h params = do
     (_, c) <- liftResourceT $ allocate (C.connectFromHandle ctx h params) C.connectionClose
     return c
 
+-- | Create a new connection. See 'Network.Connection.connectTo'.
 connectTo :: (MonadResource m) => C.ConnectionContext -> C.ConnectionParams -> m C.Connection
 connectTo ctx params = do
     (_, c) <- liftResourceT $ allocate (C.connectTo ctx params) C.connectionClose
